de6895462136871dc961f140cd2cdbcaa42be44e
= <inman.turbo@gmail.com>
2024-07-31 16:17:52 -0400

    work in progress

--- src/StorableSignature.php
+++ src/StorableSignature.php
@@ -2,12 +2,9 @@
 
 namespace Envor\Libstream;
 
-use Spatie\EventSourcing\AggregateRoots\AggregateRoot;
 use Spatie\EventSourcing\Commands\AggregateUuid;
-use Spatie\EventSourcing\Commands\HandledBy;
-use Spatie\EventSourcing\StoredEvents\ShouldBeStored;
 
-#[HandledBy(AggregateRoot::class)]
+/** #[HandledBy(AggregateRoot::class)] */
 interface StorableSignature
 {
     public function __construct(
